268f6d03e87bc23bbb3dc0f0d68e1fd0d9cd6b55
Gülzar <gulaidameer@gmail.com>
2022-12-24 11:05:24 +0400

    Header part created and completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import ReactDOM from "react-dom";
 
 function App() {
   return (
-    <div className="wrapper">
-      
-    
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" alt="Logo" />
+          <div>
+            <h3>REACT SNEAKERS</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
 
+        <div>
+          <ul className="d-flex ">
+            <li className="mr-30">
+              <img width={18} height={17} src="/img/cart.svg" alt="Cart"/>
+              <span>1205 руб.</span>
+            </li>
+            <li className="mr-30">
+              <img width={20} height={20} src="/img/user.svg" alt="User" />
+            </li>
+          </ul>
+        </div>
+      </header>
 
-    
+      <div className="content p-40">
+        <div className="d-flex  justify-between align-center mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex ">
+            <img src="/img/search.svg" alt="Search"/>
+            <input  placeholder="Поиск..."/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
