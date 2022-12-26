7e1892ce216dc0ba87949e8fdeb3b71b5513b2e7
Gülzar <gulaidameer@gmail.com>
2022-12-27 01:11:53 +0400

    css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import 'macro-css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import "macro-css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
